d78d46e193beaf2ab2ecc6f03256fcc37a8ada2b
Masaaki Goshima <goccy54@gmail.com>
2022-08-25 18:16:28 +0900

    add nil guard to walk method

--- resolved_ast/walk.go
+++ resolved_ast/walk.go
@@ -5,6 +5,9 @@ func Walk(n Node, cb func(Node) error) error {
 	if err := cb(n); err != nil {
 		return err
 	}
+	if n == nil {
+		return nil
+	}
 	for _, nn := range n.ChildNodes() {
 		if err := Walk(nn, cb); err != nil {
 			return err
